0a8f8cda185c8b7883e9825309e1859c2b3d14e3
dykimUD4M <160818411+dykimUD4M@users.noreply.github.com>
2024-12-05 10:06:33 +0900

    UIManager

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -13,5 +13,41 @@ public enum UIEvent
 
 public class UIManager : Singleton<UIManager>
 {
-    
+    private Dictionary<UIEvent, Action<EventParam>> eventDictionary = new Dictionary<UIEvent, Action<EventParam>>();
+
+    public void ListenEvent(UIEvent eventName, Action<EventParam> action)
+    {
+        if (eventDictionary.ContainsKey(eventName))
+        {
+            eventDictionary[eventName] += action;
+        }
+        else
+        {
+            eventDictionary.Add(eventName, action);
+        }
+    }
+
+    public void RemoveEvent(UIEvent eventName, Action<EventParam> action)
+    {
+        if (eventDictionary.ContainsKey(eventName))
+        {
+            eventDictionary[eventName] -= action;
+        }
+        else
+        {
+            Debug.LogError($"There is no event function to delete. | EventName : {eventName}");
+        }
+    }
+
+    public void TriggerEvent(UIEvent eventName, EventParam param)
+    {
+        if (eventDictionary.ContainsKey(eventName))
+        {
+            eventDictionary[eventName]?.Invoke(param);
+        }
+        else
+        {
+            Debug.LogError($"There is no event function to trigger. | EventName : {eventName}");
+        }
+    }
 }
